14504636398f217d114cd8a18a1821944c476f8b
Thiago Jacinto de Oliveira Andrade <139819387+Jacintones@users.noreply.github.com>
2024-09-03 14:59:06 -0300

    Update L4Q1.java

--- src/ArvoreBinaria/L4Q1.java
+++ src/ArvoreBinaria/L4Q1.java
@@ -1,5 +1,3 @@
-package L4;
-
 public class L4Q1 {
 	
 	public static No busca(int chave, No no) {
@@ -47,6 +45,115 @@ public class L4Q1 {
 		}
 	}
 	
+	public static No encontrarMinimo(No no) {
+		while (no.esquerda != null) {
+			no = no.esquerda;
+		}
+		return no;
+	}
+	
+	public static No encontrarMaximo(No no) {
+		while (no.direita != null) {
+			no = no.direita;
+		}
+		return no;
+	}
+	
+	public static No encontrarSucessor(No no) {
+		if (no.direita != null) {
+			return encontrarMinimo(no.direita);
+		}
+		else {
+			No pai = no.pai;
+			while(pai != null && no.chave == pai.direita.chave) {
+				no = pai;
+				pai = pai.pai;
+			}
+			return pai;
+		}
+	}
+	
+	public static void removerSemFilhos(No no, ArvoreBin arvore) {
+		No pai = no.pai;
+		if (pai != null) {
+			if(pai.chave >= no.chave) {
+				pai = pai.esquerda = null;
+			}else {
+				pai.direita = null;
+			}
+		}else {
+			arvore.raiz = null;
+		}
+		no.pai = null;
+	}
+	
+	public static void removerUnicoFilho(No no, ArvoreBin arvore) {
+		No pai = no.pai;
+		No filho = null;
+		
+		if (no.esquerda != null) {
+			filho = no.esquerda;
+			no.esquerda = null;
+		}else {
+			filho = no.direita;
+			no.direita = null;
+		}
+		
+		if(pai != null) {
+			filho.pai = pai;
+			if (filho.chave <= pai.chave) {
+				pai.esquerda = filho;
+			}else {
+				pai.direita = filho;
+			}
+		}else {
+			arvore.raiz = filho;
+		}
+		no.pai = null;
+	}
+	
+	public static void removerComDoisFilhos(No no, ArvoreBin arvore) {
+		No sucessor = encontrarSucessor(no);
+		sucessor = removerArvoreBinaria(sucessor.chave, arvore);
+		sucessor.pai = no.pai;
+		no.pai = null;
+		sucessor.esquerda = no.esquerda;
+		no.esquerda = null;
+		sucessor.direita = no.direita;
+		no.direita = null;
+		No pai = sucessor.pai;
+		
+		if (pai != null) {
+			if (pai.chave >= sucessor.chave) {
+				pai.esquerda = sucessor;
+			}else {
+				pai.direita = sucessor;
+			}
+		}else {
+			arvore.raiz = sucessor;
+		}
+		sucessor.esquerda.pai = sucessor;
+		if (sucessor.direita != null) {
+			sucessor.direita.pai = sucessor;
+		}
+	}
+	
+	public static No removerArvoreBinaria(int chave, ArvoreBin arvore) {
+		No atual = busca(chave, arvore.raiz);
+		if (atual == null) {
+			System.out.println("Nó com chave: "+chave+ " não existe na árvore!");
+		}else {
+			if (atual.esquerda == null && atual.direita == null) {
+				removerSemFilhos(atual, arvore);
+			}else if(atual.esquerda != null && atual.direita !=null) {
+				removerComDoisFilhos(atual, arvore);
+			}else {
+				removerUnicoFilho(atual, arvore);
+			}
+		}
+		return atual;
+	}
+	
 	public static void main(String[] args) {
 		ArvoreBin arvore = new ArvoreBin();
 		No no1 = new No(1);
@@ -58,6 +165,8 @@ public class L4Q1 {
 		inserir(no4, arvore);
 		inserir(no5, arvore);
 		inserir(no2, arvore);
+		
+		removerArvoreBinaria(4, arvore);
 		
 		imprimirEmOrdem(arvore.raiz);
 	}
